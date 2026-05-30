2845b3a245c6926c1e8156fb23b8ced79d16e941
Paloma-Lennon <l22940401@tlajomulco.tecnm.mx>
2026-05-30 15:00:57 -0600

    Primer commit QNN

---
+++ script.js
@@ -0,0 +1,8 @@
+// MENÚ HAMBURGUESA
+
+const menuToggle = document.getElementById("menu-toggle");
+const nav = document.getElementById("nav");
+
+menuToggle.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
